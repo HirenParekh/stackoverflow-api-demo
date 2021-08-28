9ed5586ccab0e4923f419ad31e9c478650a11929
HirenParekh <parekhhiren@outlook.com>
2021-08-29 00:18:40 +0530

    firebase package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {AuthModule} from './auth/auth.module';
-import {ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {HomeModule} from './home/home.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeModule } from './home/home.module';
+import { AuthModule } from './auth/auth.module';
+
+
 
 @NgModule({
   declarations: [
--- src/app/questions/questions.module.ts
+++ src/app/questions/questions.module.ts
@@ -1,9 +1,9 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {QuestionsRoutingModule} from './questions-routing.module';
-import {QuestionsComponent} from './components/questions/questions.component';
-import {QuestionsCommonModule} from '../common/questions-common/questions-common.module';
-import {QuestionsService} from './services/questions.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuestionsRoutingModule } from './questions-routing.module';
+import { QuestionsComponent } from './components/questions/questions.component';
+import { QuestionsCommonModule } from '../common/questions-common/questions-common.module';
+import { QuestionsService } from './services/questions.service';
 
 @NgModule({
   declarations: [QuestionsComponent],
